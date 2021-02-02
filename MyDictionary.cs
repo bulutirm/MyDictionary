6c25329189972c8d1e02eafcde0cec721172d8f3
Irem Bulut <bulutirm@hotmail.com>
2021-02-02 04:09:29 +0300

    Create MyDictionary.cs

---
+++ MyDictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MyDictionary
+{
+    public class MyDictionary <T>
+    {
+        T[] words;
+
+        public MyDictionary()
+        {
+            words = new T[0];
+        }
+
+        public void Add(T word)
+        {
+            T[] tempDictionary = words;
+            words = new T[words.Length + 1];
+            for(int i = 0; i < tempDictionary.Length; i++)
+            {
+                tempDictionary[i] = words[i];
+            }
+            words[words.Length - 1] = word;
+        }
+    }
+}
